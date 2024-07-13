8f57947762a679c972d7d26ce72a3b1f97ded0e5
Ramjan Ali <ramjan.ali.ashik@gmail.com>
2024-07-13 17:28:36 +0100

    expecting file in the filesystem instead of classpath; updating readme.md file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,12 @@ import service.ConsoleOutputService;
 import service.SessionAnalyzerService;
 
 import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.Map;
 
-import static util.AppUtils.isEmpty;
-import static util.AppUtils.isNull;
 import static util.AppUtils.getFileName;
+import static util.AppUtils.isEmpty;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,21 +16,23 @@ public class Main {
             System.err.println("No arguments provided");
             return;
         }
+
         String fileName = getFileName(args);
         if (isEmpty(fileName)) {
             System.err.println("No .txt file provided in arguments");
             return;
         }
 
+        File file = new File(fileName);
+        if (!file.exists()) {
+            System.err.println("File '" + file + "' does not exist!");
+            return;
+        }
+
         SessionAnalyzerService sessionAnalyzerService = new SessionAnalyzerService();
         ConsoleOutputService consoleOutputService = new ConsoleOutputService();
 
-        try (InputStream inputStream = Main.class.getResourceAsStream(fileName)) {
-            if (isNull(inputStream)) {
-                System.err.println("Sorry! Something bad happened with the file data, aborting the program");
-                return;
-            }
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             Map<String, UsageSummary> usageSummaryMap = sessionAnalyzerService.analyzeSession(bufferedReader);
             consoleOutputService.printUsageSummary(usageSummaryMap);
         } catch (Exception ex) {
